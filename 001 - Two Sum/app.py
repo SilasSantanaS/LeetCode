34314030f7de936f20f736ffe496e00477bd53e3
Silas Santana <silasuper3@gmail.com>
2020-02-19 21:43:45 -0300

    001 - Two Sum

--- 001 - Two Sum/app.py
+++ 001 - Two Sum/app.py
@@ -1,1 +1,11 @@
-print('Testing')
+def main():
+    nums = [2, 7, 11, 15]
+    target = 9
+
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+
+
+print(main())
