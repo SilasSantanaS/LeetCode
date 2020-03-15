6bec8b446db9b5495fe41c2087cdc435b092dd6c
Silas Santana <silasuper3@gmail.com>
2020-03-15 13:32:54 -0300

    Two sum - second commit

--- 001 - Two Sum/app.py
+++ 001 - Two Sum/app.py
@@ -1,11 +1,23 @@
-def main():
+'''def main(): SLOWER CODE
     nums = [2, 7, 11, 15]
     target = 9
 
     for i in range(len(nums)):
         for j in range(i + 1, len(nums)):
             if nums[i] + nums[j] == target:
-                return [i, j]
+                return [i, j]'''
+
+
+def main():  # FASTER CODE
+    nums = [2, 7, 11, 15]
+
+    target = 9
+    dictionary = dict()
+
+    for i in range(len(nums)):
+        if((target - nums[i]) in dictionary):
+            return [dictionary[target - nums[i]], i]
+        dictionary[nums[i]] = i
 
 
 print(main())
