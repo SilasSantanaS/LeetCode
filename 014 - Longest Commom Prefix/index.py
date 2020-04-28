bc10fa2c17a42909fae35ac2f509e903da1408ea
Silas Santana <silasuper3@gmail.com>
2020-04-28 10:08:24 -0300

    014 - Longest Commom Prefix

--- 014 - Longest Commom Prefix/index.py
+++ 014 - Longest Commom Prefix/index.py
@@ -1,37 +1,19 @@
-def minAndMax(str1, str2):
-    if len(str1) < len(str2):
-        return str1, str2
-    else:
-        return str2, str1
-
-
 def longestCommonPrefix(strs):
-    parcial = ""
     if len(strs) == 0:
         return ""
-    if len(strs) == 1:
-        return strs[0]
 
-    shorter, longer = minAndMax(strs[0], strs[1])
+    prefix = strs[0]
+    for i in range(1, len(strs)):
+        counter = 0
+        for j in range(len(strs[i])):
+            if prefix[: j + 1] != strs[i][:j + 1]:
+                counter += 1
+                prefix = strs[i][:j]
+                break
+        if counter == 0:
+            prefix = strs[i]
 
-    for i in range(len(shorter)):
-        if shorter[i] != longer[i]:
-            break
-        parcial += shorter[i]
-
-    if len(strs) > 2:
-        for i in range(2, len(strs)):
-            shorter, longer = minAndMax(parcial, strs[i])
-            if len(shorter) == 0:
-                return ""
-            for j in range(len(shorter)):
-                if shorter[j] != longer[j]:
-                    parcial = shorter[:j]
-                    break
-
-    return parcial
+    return prefix
 
 
-#print(longestCommonPrefix(["flower", "flow", "flight"]))
-
-print(longestCommonPrefix(["ac", "ac", "a", "a"]))
+print(longestCommonPrefix(["flower", "flow", "flight"]))
